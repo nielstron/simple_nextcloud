0ed446d73534cf7d192e6ecddcc38def4df5b8e1
Niels <niels.muendler@inf.ethz.ch>
2026-08-20 15:10:13 +0200

    Release version 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ch.niels.goodnextcloud"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.2"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
